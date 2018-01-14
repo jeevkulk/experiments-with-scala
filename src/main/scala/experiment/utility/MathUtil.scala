e2e9f193b6b0f8cbf370a075113a6ba2e11e5b9e
jeevkulk <jeevanakulkarni@gmail.com>
2018-01-14 23:27:47 +0530

    RationalNumber program in scala

---
+++ src/main/scala/experiment/utility/MathUtil.scala
@@ -0,0 +1,14 @@
+package experiment.utility
+
+object MathUtil {
+    def main (args : Array[String]): Unit = {
+        println(new MathUtil().gcd(42, 24))
+    }
+}
+
+class MathUtil {
+
+    def gcd(a : Int, b : Int) : Int = {
+        if (b == 0) a else gcd (b, a % b)
+    }
+}
---
+++ src/main/scala/experiment/utility/RationalNumber.scala
@@ -0,0 +1,32 @@
+package experiment.utility
+
+object RationalNumber {
+    def main (args : Array[String]): Unit = {
+        var a = new RationalNumber(5, 9)
+        println(a add (5, 9))
+    }
+}
+
+class RationalNumber(n : Int, d : Int) {
+    require (d != 0)
+
+    private var g = gcd (n, d)
+    val numerator = n / g
+    val denominator = d / g
+
+    def this(n: Int) = this(n, 1)
+
+    def add (n1 : Int, d1 : Int) : RationalNumber = {
+        new RationalNumber (numerator * d1 + n1 * denominator, denominator * d1)
+    }
+
+    def + (n1 : Int, d1 : Int) : RationalNumber = {
+        new RationalNumber (numerator * d1 + n1 * denominator, denominator * d1)
+    }
+
+    def gcd (n : Int, d : Int ) : Int = {
+        if (d == 0) n else gcd (d, n % d)
+    }
+
+    override def toString: String = this.numerator + " / " + this.denominator
+}
